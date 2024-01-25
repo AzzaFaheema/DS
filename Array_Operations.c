ee8cab451aa213d0a4c760bf22f608e7fbb977d9
AzzaFaheema <150208608+AzzaFaheema@users.noreply.github.com>
2024-01-25 21:18:34 +0530

    Array_Operations.c

--- Array_Operations.c
+++ Array_Operations.c
@@ -1,121 +1,128 @@
 #include<stdio.h>
 #include<stdlib.h>
+int i,n,a[30];
+int insert();
+int delete();
 int search();
 int display();
 int sort();
-int insert();
-int delete();
-void menu();
-int choice,i,j=0,a[50],b[10],s;
-void main(){
-	printf("Enter the size of the array:");
-	scanf("%d",&s);
-	printf("Enter the elements of array:");
-	for(i=0;i<s;i++){
-		scanf("%d",&a[i]);
-	}
-	menu();
+int menu();
+void main()
+{
+    printf("Enter size of array:\n");
+    scanf("%d",&n);
+    printf("Enter elements of array:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    menu();
 }
-void menu(){
-	printf("\n1.Insert");
-	printf("\n2.Delete");
-	printf("\n3.Sort");
-	printf("\n4.Search");
-	printf("\n5.Display");
-	printf("\n6.Exit");
-	printf("\nEnter your choice:");
-	scanf("%d",&choice);
-	switch(choice)
-	{
-		case 1:insert();
-		break;
-		case 2:delete();
-		break;
-		case 3:sort();
-		break;
-		case 4:search();
-		break;
-		case 5:display();
-		break;
-		case 6:exit(1);
-	}
+int insert()
+{
+    int num,pos;
+    printf("\nEnter the number to be inserted:");
+    scanf("%d",&num);
+    printf("\nEnter position:");
+    scanf("%d",&pos);
+    for(i=n-1;i>=pos-1;i--)
+    {
+	    a[i+1]=a[i];
+    }
+    a[pos-1]=num;
+    n++;
+    printf("\nArray after Insertion:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    menu();
 }
-int search(){
-	int ele,loc;
-	printf("Enter element to be searched:");
-	scanf("%d",&ele);
-	printf("Element found at:");
-	for(i=0;i<s;i++)
-	{
-	if(ele==a[i])
-		printf("%d",i+1);
-	}
-	menu();
-	return 0;
+int delete()
+{
+    int pos,item;
+    printf("\nEnter position to delete:");
+    scanf("%d",&pos);
+    item=a[pos-1];
+    for(i=pos-1;i<n-1;i++)
+    {
+        a[i]=a[i+1];
+    }
+    n--;
+    printf("\nDeleted %d", item);
+    printf("\nArray after Deletion:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    menu();
 }
-int sort(){
-	printf("\nCurrent array:");
-	for(i=0;i<s;i++)
-		printf("\t%d\t",a[i]);
+int sort()
+{
+    int temp,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+             temp=a[i];
+             a[i]=a[j];
+             a[j]=temp;
+            } 
+        }
+        
+    }
+    printf("\nSorted Array:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    menu();
+} 
+int display()
+{   
+    printf("\nArray:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    menu();
+}
+int search()
+{
+    int ele;
+    printf("\nEnter element to be searched:");
+    scanf("%d",&ele);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==ele)
+        {
+            printf("Element located at %d", i+1);
+        }
+    }
+    menu();
+}
+int menu()
+{
+    int ch;
+    printf("\n1.Insertion \n2.Deletion \n3.Search \n4.Display \n5.Sort \n6.Exit");
+    printf("\nEnter Choice:");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1: insert();
+                break;
+        case 2: delete();
+                break;
+        case 3: search();
+                break;
+        case 4: display();
+                break;
+        case 5: sort();
+                break;
+        case 6: printf("Exiting");
+                exit(1);
+    }
+}
 
-	for(i=0;i<s;i++)
-	{
-		for(j=i+1;j<s;j++)
-			if(a[i]>a[j])
-			{
-				a[i]=a[i]+a[j];
-				a[j]=a[i]-a[j];
-				a[i]=a[i]-a[j];
-			}
-	}
-	printf("\nSorted array:\n");
-	for(i=0;i<s;i++)
-		printf("%d\t",a[i]);
-	menu();
-	return 0;
-}
-int insert(){
-	int loc,ele;
-	printf("\nEnter the location:");
-	scanf("%d",&loc);
-	printf("Enter the element to be inserted:");
-	scanf("%d",&ele);
-	for(i=s;i>loc-1;i--)
-		a[i]=a[i-1];
-		a[loc-1]=ele;
-	printf("\nElement inserted");
-	printf("\nArray after insertion\n");
-	s++;
-	for(i=0;i<s;i++)
-		printf("%d\t",a[i]);
-	menu();
-	return 0;
-}
-int delete(){
-	int loc;
-	printf("\nEnter location:");
-	scanf("%d",&loc);
-	for(i=loc;i<s-1;i++)
-		a[i]=a[i+1];
-		s--;
-	printf("\nElement deleted\n");
-	printf("\nNew array after deletion\n");
-	for(i=0;i<s;i++)
-		printf("\t%d\t",a[i]);
-	menu();
-	return 0;
-}
-
-int display(){
-	int i;
-	if(s==0)
-		printf("\nNo elements to display");
-	else
-		printf("Array elements are:");
-		for(i=0;i<s;i++)
-		{
-			printf("%d\t",a[i]);
-		}
-	menu();
-	return 0;
-}
